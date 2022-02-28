122576cdae9afd83eb95e4d6bde4e1e80607bf0e
Alba Martínez <Alba Martnez@Alba>
2022-02-28 20:25:08 +0100

    Subir archivos a Github

---
+++ pruebagithub/src/pruebagithub/Pruebagithub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagithub;
+
+/**
+ *
+ * @author Alba Martínez
+ */
+public class Pruebagithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
